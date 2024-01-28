e5cf4dd0afa02b0a8635f6eb0b8c58e09b87c6f6
Christian Nickerson <christian_nickerson@hotmail.com>
2024-01-28 12:33:50 +0000

    formatted time in logging

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,11 +1,14 @@
 package logging
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 var LoggingConfig = logger.Config{
-	Format:     "${time} ${locals:requestid} ${status} | ${method} ${path} ${error}\n",
-	TimeFormat: "15:04:05",
-	TimeZone:   "UTC",
+	Format:       "[${time}] ${locals:requestid} ${status} | ${method} ${path} ${error}\n",
+	TimeFormat:   "15:04:05 15:04:05.000",
+	TimeInterval: time.Millisecond,
+	TimeZone:     "UTC",
 }
